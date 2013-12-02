9d5ce9e85a3778e9ee5a6b8ef9c7336b1d584445
Tony Tiratay <tony.tiratay@gmail.com>
2013-12-02 17:20:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Greenway::Application.config.secret_key_base = 'cc48fd11bf627668ce4db655bc056598de1579b7f16487a822a251bbfeb4ed3f2f7a03977d2517a65eabbc9225759dc2551fb0cf688b94bdf9fb59ed350ac9d8'
