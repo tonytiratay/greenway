5b38ddcb18d33b2a3b5014636a5b92a1b212e49b
tonytiratay <tony.tiratay@gmail.com>
2014-01-07 10:33:51 +0100

    Better landing pages, add devise and add home link on logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Greenway::Application.routes.draw do
+  devise_for :users
   resources :activites
   root 'pages#accueil'
   # The priority is based upon order of creation: first created -> highest priority.
