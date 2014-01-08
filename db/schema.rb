9a51d9d02dd153ad30691c097309e15d05d93412
tonytiratay <tony.tiratay@gmail.com>
2014-01-08 14:32:23 +0100

    Activate user connexion and customize login and sign-up form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140107083713) do
+ActiveRecord::Schema.define(version: 20140108104434) do
 
   create_table "activites", force: true do |t|
     t.string   "titre"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20140107083713) do
     t.string   "adresse"
     t.date     "datedebut"
     t.date     "datefin"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tests", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
